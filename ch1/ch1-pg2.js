d42dce9d74d4718f2e77bf5c948d7e2c741c6051
shuishiyuan <liubinbupt@qq.com>
2020-09-08 10:29:43 +0800

    Make the output format of ch1-pg2.js to a more convenient way

--- ch1/ch1-pg2.js
+++ ch1/ch1-pg2.js
@@ -1,4 +1,4 @@
-console.log(8 / 5);
-console.log(8.0 /5);
-console.log(8 / 5.0);
-console.log(8.0 / 5.0);
+console.log("The result of 8 / 5 is:", 8 / 5);
+console.log("The result of 8.0 / 5 is:", 8.0 /5);
+console.log("The result of 8 / 5.0 is:", 8 / 5.0);
+console.log("The result of 8.0 / 5.0 is:", 8.0 / 5.0);
