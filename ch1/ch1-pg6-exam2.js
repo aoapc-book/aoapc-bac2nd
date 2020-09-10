eb8509c3fb416f620208f324108284fd2b9a0678
b-liu <b-liu@nri.co.jp>
2020-09-10 10:31:30 +0800

    Start the ES6 version of reverse input digit

---
+++ ch1/ch1-pg6-exam2.js
@@ -0,0 +1,15 @@
+const lineReader = require("readline");
+const consLineRd = lineReader.createInterface({
+    input:process.stdin
+});
+consLineRd.on("close", () => {
+    process.exit();
+});
+
+let num;
+
+consLineRd.on("line", input => {
+    num = parseInt(input);
+    console.log(num % 10, num / 10 % 10, num / 100);
+    consLineRd.close();
+});
