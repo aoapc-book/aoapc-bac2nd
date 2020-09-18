e52bd0aa1fa15da6680dbeec362c654a766846a5
b-liu <b-liu@nri.co.jp>
2020-09-18 10:29:21 +0800

    Debugging in the method 2 of reverse a and b

---
+++ ch1/ch1-pg9-exam3.js
@@ -0,0 +1,26 @@
+const lnrd = require("readline");
+const cslLnrd = lnrd.createInterface({
+    input: process.stdin
+});
+cslLnrd.on("close", () => {
+    process.exit();
+});
+
+let cnt = 1;
+let a = 0;
+let b = 0;
+
+cslLnrd.on("line", input => {
+    if (cnt == 1) {
+        a = input;
+    }
+    if (cnt == 2) {
+        b = input;
+        b = a + b;
+        a = b - a;
+        b = b - a;
+        console.log("Reverse a and b method 2:",`${a}\t${b}`);
+        cslLnrd.close();
+    }
+    cnt++;
+});
