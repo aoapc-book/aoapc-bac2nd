82c41c37975232c745f18904d9ba37b549fd6a06
b-liu <b-liu@nri.co.jp>
2020-10-09 13:52:04 +0800

    Finished: maniyaku method of change a and b

--- ch1/ch1-pg10-exam3.js
+++ ch1/ch1-pg10-exam3.js
@@ -1,12 +1,24 @@
-const lineReader = inport('read-line');
-const consolLineReader = lineReader.open({
-    intput: stdin
-)}
-consolLineReader.('close')( () => {
+const lineReader = require("readline");
+const consolLineReader = lineReader.createInterface({
+    input: process.stdin
+})
+consolLineReader.on("close", () => {
     process.exit();
 });
 
 let a;
 let b;
+let counter = 1;
 
+consolLineReader.on("line", input => {
+    if (counter == 1) {
+        a = input;
+    }
+    if (counter == 2) {
+        b = input;
+        console.log("The reverse result of input value is:", `${b}\t${a}`);
+        consolLineReader.close();
+    }
+    counter++;
+});
 
