0880245055d8e1d5d6b96e9d5866820aae65f1e6
b-liu <b-liu@nri.co.jp>
2020-11-04 19:21:17 +0800

    Started pure way of sorted program with equality

---
+++ ch1/ch1-pg13-exam5.js
@@ -0,0 +1,31 @@
+const lineReader = require("readline");
+const consoleLR = lineReader.createInterface({
+    input: process.stdin
+});
+consoleLR.on('close', () => {
+    process.exit();
+});
+consoleLR.on('open', () => {
+    console.log("Hello ch1-pg13-exam5.js");
+});
+
+let a;
+let b;
+let c;
+
+let counter = 1;
+
+consoleLR.on('line', (input) => {
+    if (counter === 1) {
+        a = input;
+    }
+    if (counter === 2) {
+        b = input;
+    }
+    if (counter === 3) {
+        c = input;
+        console.log("The input sequence is:", `${a}\t${b}\t${c}`);
+        consoleLR.close();
+    }
+    counter++;
+});
