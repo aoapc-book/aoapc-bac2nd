7d8984cceabc98e43ebc766164ada88a51fb31bc
b-liu <b-liu@nri.co.jp>
2020-11-11 18:59:59 +0800

    Finished: Accumlation from 1 to n

---
+++ ch1/ch1-pg18-exer3.js
@@ -0,0 +1,24 @@
+console.log('Accumlation from 1 to n');
+const lnreader = require("readline");
+const cmdLnreader = lnreader.createInterface({
+    input: process.stdin
+});
+cmdLnreader.on('close', () => {
+    process.exit()
+});
+
+let n;
+let sum;
+
+cmdLnreader.on('line', (input) => {
+    n = parseInt(input);
+    sum = n;
+    console.log('The input number is:', `\n${n}`);
+    console.log('');
+    console.log(`The accumlation from 1 to ${n} is:`);
+    while (--n) {
+        sum += n;
+    }
+    console.log(`${sum}`);
+    cmdLnreader.close();
+});
