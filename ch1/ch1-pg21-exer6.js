90941322604d228257c0bcb6aa539de0f6595826
b-liu <b-liu@nri.co.jp>
2020-11-13 08:06:27 +0800

    Finished: Judging the right angled triangle

--- ch1/ch1-pg21-exer6.js
+++ ch1/ch1-pg21-exer6.js
@@ -14,6 +14,9 @@ let c;
 counter = 1;
 
 cmdReadLn.on('line', (input) => {
+    if (input === 'Q') {
+        cmdReadLn.close();
+    }
     if (counter === 1) {
         a = parseInt(input);
     }
@@ -22,5 +25,25 @@ cmdReadLn.on('line', (input) => {
     }
     if (counter === 3) {
         c = parseInt(input);
+        console.log(`The input 3 digits are: ${a}\t${b}\t${c}`);
+        if (a > c) {
+            a = a + c;
+            c = a - c;
+            a = a - c;
+        }
+        if (b > c) {
+            b = b + c;
+            c = b - c;
+            b = b - c;
+        }
+        if (a + b <= c) {
+            console.log("This is not a triangle");
+        }else if (Math.pow(a, 2) + Math.pow(b, 2) != Math.pow(c, 2)) {
+            console.log("This is a none right angled triangle");
+        } else {
+            console.log("This is a right angled triangle");
+        }
+        counter = 0;
     }
-})
+    counter++;
+});
